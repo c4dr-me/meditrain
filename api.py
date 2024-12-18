5e8ebdd01b30c1c2cfb631ff03686672eee313ff
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-12-19 01:06:17 +0530

    initial

--- api.py
+++ api.py
@@ -71,7 +71,7 @@ def test_users():
 
 port = int(os.getenv('PORT', 5000))
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 # print(chat_completion.choices[0].message.content)
 # print(chat_completion)
