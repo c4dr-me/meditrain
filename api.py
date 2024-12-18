6e36405904496ca9b180bf8e04018b55328c3750
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-12-18 23:52:10 +0530

    initial

---
+++ api.py
@@ -0,0 +1,78 @@
+import os 
+from dotenv import load_dotenv
+from groq import Groq
+import requests
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from backend.chatbot import get_chatbot_response
+
+
+load_dotenv()
+
+app = Flask(__name__)
+allowed_origin = os.getenv('ALLOWED_ORIGIN')
+CORS(app, resources={r"/*": {"origins": allowed_origin}})
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+def get_response(text):
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": """You are a Doctor and you are advising a patient on the importance of regular health check-ups."""
+        },
+        {
+            "role": "user",
+            "content": text,
+        }
+    ],
+    model="llama3-8b-8192",
+)
+    return chat_completion.choices[0].message.content
+
+
+@app.route('/', methods=['GET'])
+def checkHealth():
+    try:
+        return jsonify({"status": "Health check"}), 200
+    except Exception as e:
+        return jsonify({"error": "An error occurred"}), 500
+
+
+@app.route('/response', methods=['POST'])
+def response():
+    try:
+        data = request.get_json()
+        query = data.get('query')
+        response = get_chatbot_response(query)
+        return jsonify({"response": response}), 200
+
+    except Exception as e:
+        return jsonify({"error": "An error occurred"}), 500    
+    
+def get_users():
+    url = "https://randomuser.me/api/?results=10"
+    response = requests.get(url)
+    return response.json()
+
+
+@app.route("/test_users", methods=["GET"])
+def test_users():
+    try:
+        response = get_users()
+        users = response["results"]
+        return jsonify(users)
+
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# print(chat_completion.choices[0].message.content)
+# print(chat_completion)
